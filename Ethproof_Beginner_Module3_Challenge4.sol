84c40864020e5b182de0d7a308e479f3014a1e58
raghav0004 <128291547+raghav0004@users.noreply.github.com>
2023-08-18 13:30:16 +0530

    Create Ethproof_Beginner_Module3_Challenge4.sol

---
+++ Ethproof_Beginner_Module3_Challenge4.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract ArithmeticOperations {
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+    function subtract(uint256 a, uint256 b) public pure returns (uint256) {
+        return a - b;
+    }
+
+    function multiply(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
+    }
+
+    function divide(uint256 a, uint256 b) public pure returns (uint256) {
+        require(b != 0, "Division by zero is not allowed");
+        return a / b;
+    }
+}
